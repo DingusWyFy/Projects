e8e072dca1e2656dd52ef15e7e0e9d32f00d4624
DingusWyFy <103014721+DingusWyFy@users.noreply.github.com>
2022-07-22 15:11:14 -0700

    Initial

---
+++ variables.py
@@ -0,0 +1,7 @@
+# This is a program written in Python
+# The purpose of this script is to define a variable twice. And print said variable twice with a different value.
+
+message = "Hello end user, this is the first prompt."
+print(message)
+message = "I hope you enjoy the second prompt as much as the first."
+print(message)
